156bd3beb01122114d292b80bacb5b152410b18c
Chameleon Cloud User <zjulxy@163.com>
2020-12-03 03:44:53 +0000

    pure3miss

---
+++ pure3miss_1_core.sh
@@ -0,0 +1,28 @@
+
+./build_champsim.sh no
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/403.gcc-17B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/429.mcf-22B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/436.cactusADM-1804B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/437.leslie3d-265B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/459.GemsFDTD-1491B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/462.libquantum-714B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/473.astar-359B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/482.sphinx3-1100B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/603.bwaves_s-2609B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/621.wrf_s-6673B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/623.xalancbmk_s-202B.champsimtrace.xz
+
+./run_champsim.sh ./bin/perceptron-no-no-no-lru-1core 10 50 ./traces/654.roms_s-293B.champsimtrace.xz
+
+
